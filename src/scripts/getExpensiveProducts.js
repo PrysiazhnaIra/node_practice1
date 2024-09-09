9cdc91baf8b27d8504297ad9599e11a8975d6e77
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-09 15:32:28 +0300

    getExpensiveProducts

---
+++ src/scripts/getExpensiveProducts.js
@@ -0,0 +1,17 @@
+import fs from "node:fs/promises";
+import { DB_PATH } from "../constants/path.js";
+
+const getExpensiveProducts = async () => {
+  const PRICE = 300;
+  try {
+    const data = await fs.readFile(DB_PATH, "utf-8");
+    const products = JSON.parse(data);
+    const expensiveProducts = products.filter(
+      (product) => product.price >= PRICE
+    );
+    console.log(expensiveProducts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getExpensiveProducts();
